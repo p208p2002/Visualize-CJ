0e16a5a6557a340e8c53cebfdea663ee499a54ab
Philip <p208p2002@gmail.com>
2020-03-27 14:13:55 +0800

    no message

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@
 (docker stop vcj_server&&docker rm vcj_server);
 
 # build client image
-cd client
 echo "REACT_APP_API_HOST=${REACT_APP_API_HOST}:${SERVER_PORT}" >> .env # setting .env
-docker-build.sh
+cd client&&sh docker-build.sh
 cd ../
 
 # build server image
 cd server&&sh docker-build.sh
+cd ../
 
 # run
 docker run -itd -p ${CLIENT_PORT}:3000 --name=vcj_client vcj_client
