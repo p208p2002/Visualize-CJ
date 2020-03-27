c6d717e03bdc57ed26c850f22065e84107e640e9
Philip <p208p2002@gmail.com>
2020-03-27 09:40:37 +0800

    no message

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
+# clean
+rm -rf server/
+rm -rf client/
+rm -rf trained_model/
+rm -rf trained_model.zip
+
+# clone from git
 git clone https://github.com/p208p2002/Visualize-CJ-server.git server
 git clone https://github.com/p208p2002/Visualize-CJ-client.git client
+wget http://140.120.13.242:7000/trained_model.zip
+
+# unzip & clean
+unzip trained_model.zip
+cp -r trained_model server/lib/cjbert/bert-question-answer/
+rm -rf trained_model/
+rm -rf trained_model.zip
