1d0d99d98d627043d1aa35b86b365558b888e987
Philip <p208p2002@gmail.com>
2020-03-27 12:52:04 +0800

    no message

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Visualize-CJ
 ## Init
 - `sh init.sh`
-- 配置`client/.env`
 ## Build
 - `export REACT_APP_API_HOST=API_HOST&&sh build.sh`
--- build.sh
+++ build.sh
@@ -3,15 +3,20 @@
 (docker rm vcj_client);
 (docker image rm vcj_server);
 (docker rm vcj_server);
+
+#
+export CLIENT_PORT=12500
+export SERVER_PORT=12501
+
 # build client image
 cd client
+echo "REACT_APP_API_HOST=${REACT_APP_API_HOST}:${SERVER_PORT}" >> .env # setting .env
 npm run docker-build
-echo "REACT_APP_API_HOST=${REACT_APP_API_HOST}" >> .env
 cd ../
 
 # build server image
 cd server&&sh docker-build.sh
 
 # run
-docker run -itd -p 12500:3000 --name=vcj_client vcj_client
-docker run -itd --gpus all -p 12501:8888 --name=vcj_server vcj_server
+docker run -itd -p ${CLIENT_PORT}:3000 --name=vcj_client vcj_client
+docker run -itd --gpus all -p ${SERVER_PORT}:8888 --name=vcj_server vcj_server
