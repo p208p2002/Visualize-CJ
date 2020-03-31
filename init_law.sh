454aced383d8a629cc9c74be1701aeb90427c56e
Philip <p208p2002@gmail.com>
2020-03-31 11:27:17 +0800

    no message

--- init_law.sh
+++ init_law.sh
@@ -8,7 +8,7 @@ rm -rf trained_model.zip
 
 # clone from git
 git clone -b find_law https://github.com/p208p2002/Visualize-CJ-server.git server
-git clone https://github.com/p208p2002/Visualize-CJ-client.git client
+git clone -b find_law https://github.com/p208p2002/Visualize-CJ-client.git client
 wget -O trained_model.zip http://140.120.13.242:7000/bert-base-chinese-drcd.zip
 # wget -O trained_model.zip  http://178.128.89.37:12000/bert-base-chinese-drcd.zip
 
